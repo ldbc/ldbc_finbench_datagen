a2f8bf2cd64ced73a86bd737af35dceb483a5d26
Nicole00 <anqi.wang@vesoft.com>
2022-10-18 17:07:54 +0800

    add io untils

--- src/main/scala/ldbc/finbench/datagen/io/Writer.scala
+++ src/main/scala/ldbc/finbench/datagen/io/Writer.scala
@@ -1,7 +1,33 @@
-/* Copyright (c) 2021 vesoft inc. All rights reserved.
- *
- * This source code is licensed under Apache 2.0 License.
- */
-
 package ldbc.finbench.datagen.io
-class writer {}
+
+trait Writer[S] {
+  type Data
+  def write(self: Data, sink: S): Unit
+}
+
+object Writer {
+  type Aux[S, D] = Writer[S] { type Data = D }
+  def apply[S, D](implicit r: Writer.Aux[S, D]): Writer.Aux[S, D] = implicitly[Writer.Aux[S, D]]
+
+  trait WriterOps[Data] {
+    type Sink
+    def tcInstance: Writer.Aux[Sink, Data]
+    def self: Data
+    def write(sink: Sink): Unit = tcInstance.write(self, sink)
+  }
+
+  object WriterOps {
+    type Aux[Data, S] = WriterOps[Data] { type Sink = S }
+  }
+
+  object ops {
+    import scala.language.implicitConversions
+    implicit def toWriterOps[Data, S](target: Data)(
+        implicit tc: Writer.Aux[S, Data]): WriterOps.Aux[Data, S] = new WriterOps[Data] {
+      override type Sink = S
+      override def tcInstance: Aux[S, Data] = tc
+      override def self: Data               = target
+    }
+  }
+
+}
--- src/main/scala/ldbc/finbench/datagen/io/graphs.scala
+++ src/main/scala/ldbc/finbench/datagen/io/graphs.scala
@@ -1,7 +1,21 @@
-/* Copyright (c) 2021 vesoft inc. All rights reserved.
- *
- * This source code is licensed under Apache 2.0 License.
- */
-
 package ldbc.finbench.datagen.io
-object graphs {}
+
+import ldbc.finbench.datagen.model.Mode.Raw.Layout
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.reflect.internal.Mode
+
+object graphs {
+
+  case class GraphSource[M <: Mode](implicit spark: SparkSession, en: DataFrame =:= Layout)
+      extends Reader[GraphSource[M]] {
+    @transient lazy val log: Logger = LoggerFactory.getLogger(this.getClass)
+
+    override type Ret = this.type
+
+    override def read(self: GraphSource[M]): GraphSource.this.type = ???
+
+    override def exists(self: GraphSource[M]): Boolean = ???
+  }
+}
