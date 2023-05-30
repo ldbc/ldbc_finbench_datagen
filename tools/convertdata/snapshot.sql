c6605029fff5a199220bff2857cf599829de3f63
qishipengqsp <shepherdkingqsp@gmail.com>
2023-05-30 15:34:59 +0800

    wip

--- tools/convertdata/snapshot.sql
+++ tools/convertdata/snapshot.sql
@@ -1,227 +1,227 @@
-
 --- Person
-COPY (
-SELECT
-    Person.id AS personId,
-    Person.name AS personName,
-    Person.isBlocked AS isBlocked,
-    epoch_ms(Person.createTime) AS createTime
+COPY
+(
+SELECT Person.id                   AS personId,
+       Person.name                 AS personName,
+       Person.isBlocked            AS isBlocked,
+       epoch_ms(Person.createTime) AS createTime
 FROM Person
 WHERE Person.createTime <= :start_date_long
-ORDER BY Person.createTime
-)
+ORDER BY Person.createTime )
 TO ':output_dir/snapshot/Person.:output_format';
 
 --- Company
-COPY (
-SELECT
-    Company.id AS companyId,
-    Company.name AS companyName,
-    Company.isBlocked AS isBlocked,
-    epoch_ms(Company.createTime) AS createTime
+COPY
+(
+SELECT Company.id                   AS companyId,
+       Company.name                 AS companyName,
+       Company.isBlocked            AS isBlocked,
+       epoch_ms(Company.createTime) AS createTime
 FROM Company
 WHERE Company.createTime <= :start_date_long
-ORDER BY Company.createTime
-)
+ORDER BY Company.createTime )
 TO ':output_dir/snapshot/Company.:output_format';
 
 -- Account. It has deletes.
-COPY (
-SELECT
-    Account.id AS accountId,
-    epoch_ms(Account.createTime) AS createTime,
-    Account.isBlocked AS isBlocked,
-    Account.type AS accoutType
+COPY
+(
+SELECT Account.id                   AS accountId,
+       epoch_ms(Account.createTime) AS createTime,
+       Account.isBlocked            AS isBlocked,
+       Account.type                 AS accoutType
 FROM Account
-WHERE Account.createTime <= :start_date_long AND Account.deleteTime > :start_date_long
-ORDER BY Account.createTime
-)
+WHERE Account.createTime <= :start_date_long
+  AND Account.deleteTime > :start_date_long
+ORDER BY Account.createTime )
 TO ':output_dir/snapshot/Account.:output_format';
 
 -- Loan.
-COPY (
-SELECT
-    Loan.id AS loanId,
-    Loan.loanAmount AS loanAmount,
-    Loan.balance AS balance,
-    epoch_ms(Loan.createTime) AS createTime
+COPY
+(
+SELECT Loan.id                   AS loanId,
+       Loan.loanAmount           AS loanAmount,
+       Loan.balance              AS balance,
+       epoch_ms(Loan.createTime) AS createTime
 FROM Loan
 WHERE Loan.createTime <= :start_date_long
-ORDER BY Loan.createTime
-)
+ORDER BY Loan.createTime )
 TO ':output_dir/snapshot/Loan.:output_format';
 
 -- Medium.
-COPY (
-SELECT
-    Medium.id AS mediumId,
-    Medium.type AS mediumType,
-    Medium.isBlocked AS isBlocked,
-    epoch_ms(Medium.createTime) AS createTime
+COPY
+(
+SELECT Medium.id                   AS mediumId,
+       Medium.type                 AS mediumType,
+       Medium.isBlocked            AS isBlocked,
+       epoch_ms(Medium.createTime) AS createTime
 FROM Medium
 WHERE Medium.createTime <= :start_date_long
-ORDER BY Medium.createTime
-)
+ORDER BY Medium.createTime )
 TO ':output_dir/snapshot/Medium.:output_format';
 
 -- Transfer. It has deletes.
-COPY (
-SELECT
-    Transfer.fromId AS fromId,
-    Transfer.toId AS toId,
-    Transfer.amount AS amount,
-    epoch_ms(Transfer.createTime) AS createTime
-FROM Transfer
-WHERE Transfer.createTime <= :start_date_long AND Transfer.deleteTime > :start_date_long
-ORDER BY Transfer.createTime
+COPY
+(
+    (SELECT
+        Transfer.fromId AS fromId,
+        Transfer.toId AS toId,
+        Transfer.amount AS amount,
+        epoch_ms(Transfer.createTime) AS createTime
+    FROM Transfer
+    WHERE Transfer.createTime <= :start_date_long AND Transfer.deleteTime > :start_date_long
+    )
+    UNION ALL
+    (SELECT
+        LoanTransfer.fromId AS fromId,
+        LoanTransfer.toId AS toId,
+        LoanTransfer.amount AS amount,
+        epoch_ms(LoanTransfer.createTime) AS createTime
+    FROM LoanTransfer
+    WHERE LoanTransfer.createTime <= :start_date_long  AND LoanTransfer.deleteTime > :start_date_long
+    )
+    ORDER BY createTime
 )
 TO ':output_dir/snapshot/Transfer.:output_format';
 
 -- Withdraw. It has deletes.
-COPY (
-SELECT
-    Withdraw.fromId AS fromId,
-    Withdraw.toId AS toId,
-    Withdraw.amount AS amount,
-    epoch_ms(Withdraw.createTime) AS createTime
+COPY
+(
+SELECT Withdraw.fromId               AS fromId,
+       Withdraw.toId                 AS toId,
+       Withdraw.amount               AS amount,
+       epoch_ms(Withdraw.createTime) AS createTime
 FROM Withdraw
-WHERE Withdraw.createTime <= :start_date_long AND Withdraw.deleteTime > :start_date_long
-ORDER BY Withdraw.createTime
-)
+WHERE Withdraw.createTime <= :start_date_long
+  AND Withdraw.deleteTime > :start_date_long
+ORDER BY Withdraw.createTime )
 TO ':output_dir/snapshot/Withdraw.:output_format';
 
 -- Repay. It has deletes.
-COPY (
-SELECT
-    Repay.accountId AS accountId,
-    Repay.loanId AS loanId,
-    Repay.amount AS amount,
-    epoch_ms(Repay.createTime) AS createTime
+COPY
+(
+SELECT Repay.accountId            AS accountId,
+       Repay.loanId               AS loanId,
+       Repay.amount               AS amount,
+       epoch_ms(Repay.createTime) AS createTime
 FROM Repay
-WHERE Repay.createTime <= :start_date_long AND Repay.deleteTime > :start_date_long
-ORDER BY Repay.createTime
-)
+WHERE Repay.createTime <= :start_date_long
+  AND Repay.deleteTime > :start_date_long
+ORDER BY Repay.createTime )
 TO ':output_dir/snapshot/Repay.:output_format';
 
 -- Deposit. It has deletes.
-COPY (
-SELECT
-    Deposit.loanId AS loanId,
-    Deposit.accountId AS accountId,
-    Deposit.amount AS amount,
-    epoch_ms(Deposit.createTime) AS createTime
+COPY
+(
+SELECT Deposit.loanId               AS loanId,
+       Deposit.accountId            AS accountId,
+       Deposit.amount               AS amount,
+       epoch_ms(Deposit.createTime) AS createTime
 FROM Deposit
-WHERE Deposit.createTime <= :start_date_long AND Deposit.deleteTime > :start_date_long
-ORDER BY Deposit.createTime
-)
+WHERE Deposit.createTime <= :start_date_long
+  AND Deposit.deleteTime > :start_date_long
+ORDER BY Deposit.createTime )
 TO ':output_dir/snapshot/Deposit.:output_format';
 
 -- SignIn. It has deletes.
-COPY (
-SELECT
-    SignIn.mediumId AS mediumId,
-    SignIn.accountId AS accountId,
-    epoch_ms(SignIn.createTime) AS createTime
+COPY
+(
+SELECT SignIn.mediumId             AS mediumId,
+       SignIn.accountId            AS accountId,
+       epoch_ms(SignIn.createTime) AS createTime
 FROM SignIn
-WHERE SignIn.createTime <= :start_date_long AND SignIn.deleteTime > :start_date_long
-ORDER BY SignIn.createTime
-)
+WHERE SignIn.createTime <= :start_date_long
+  AND SignIn.deleteTime > :start_date_long
+ORDER BY SignIn.createTime )
 TO ':output_dir/snapshot/MediumSignInAccount.:output_format';
 
 -- PersonInvest.
-COPY (
-SELECT
-    PersonInvest.investorId AS investorId,
-    PersonInvest.companyId AS companyId,
-    PersonInvest.ratio AS ratio,
-    epoch_ms(PersonInvest.createTime) AS createTime
+COPY
+(
+SELECT PersonInvest.investorId           AS investorId,
+       PersonInvest.companyId            AS companyId,
+       PersonInvest.ratio                AS ratio,
+       epoch_ms(PersonInvest.createTime) AS createTime
 FROM PersonInvest
 WHERE PersonInvest.createTime <= :start_date_long
-ORDER BY PersonInvest.createTime
-)
+ORDER BY PersonInvest.createTime )
 TO ':output_dir/snapshot/PersonInvestCompany.:output_format';
 
 -- CompanyInvest.
-COPY (
-SELECT
-    CompanyInvest.investorId AS investorId,
-    CompanyInvest.companyId AS companyId,
-    CompanyInvest.ratio AS ratio,
-    epoch_ms(CompanyInvest.createTime) AS createTime
+COPY
+(
+SELECT CompanyInvest.investorId           AS investorId,
+       CompanyInvest.companyId            AS companyId,
+       CompanyInvest.ratio                AS ratio,
+       epoch_ms(CompanyInvest.createTime) AS createTime
 FROM CompanyInvest
 WHERE CompanyInvest.createTime <= :start_date_long
-ORDER BY CompanyInvest.createTime
-)
+ORDER BY CompanyInvest.createTime )
 TO ':output_dir/snapshot/CompanyInvestCompany.:output_format';
 
 -- PersonApplyLoan.
-COPY (
-SELECT
-    PersonApplyLoan.personId AS personId,
-    PersonApplyLoan.loanId AS loanId,
-    epoch_ms(PersonApplyLoan.createTime) AS createTime
+COPY
+(
+SELECT PersonApplyLoan.personId             AS personId,
+       PersonApplyLoan.loanId               AS loanId,
+       epoch_ms(PersonApplyLoan.createTime) AS createTime
 FROM PersonApplyLoan
 WHERE PersonApplyLoan.createTime <= :start_date_long
-ORDER BY PersonApplyLoan.createTime
-)
+ORDER BY PersonApplyLoan.createTime )
 TO ':output_dir/snapshot/PersonApplyLoan.:output_format';
 
 -- CompanyApplyLoan.
-COPY (
-SELECT
-    CompanyApplyLoan.companyId AS companyId,
-    CompanyApplyLoan.loanId AS loanId,
-    epoch_ms(CompanyApplyLoan.createTime) AS createTime
+COPY
+(
+SELECT CompanyApplyLoan.companyId            AS companyId,
+       CompanyApplyLoan.loanId               AS loanId,
+       epoch_ms(CompanyApplyLoan.createTime) AS createTime
 FROM CompanyApplyLoan
 WHERE CompanyApplyLoan.createTime <= :start_date_long
-ORDER BY CompanyApplyLoan.createTime
-)
+ORDER BY CompanyApplyLoan.createTime )
 TO ':output_dir/snapshot/CompanyApplyLoan.:output_format';
 
 -- PersonGuaranteePerson.
-COPY (
-SELECT
-    PersonGuarantee.fromId AS fromId,
-    PersonGuarantee.toId AS toId,
-    epoch_ms(PersonGuarantee.createTime) AS createTime
+COPY
+(
+SELECT PersonGuarantee.fromId               AS fromId,
+       PersonGuarantee.toId                 AS toId,
+       epoch_ms(PersonGuarantee.createTime) AS createTime
 FROM PersonGuarantee
 WHERE PersonGuarantee.createTime <= :start_date_long
-ORDER BY PersonGuarantee.createTime
-)
+ORDER BY PersonGuarantee.createTime )
 TO ':output_dir/snapshot/PersonGuaranteePerson.:output_format';
 
 -- CompanyGuaranteeCompany.
-COPY (
-SELECT
-    CompanyGuarantee.fromId AS fromId,
-    CompanyGuarantee.toId AS toId,
-    epoch_ms(CompanyGuarantee.createTime) AS createTime
+COPY
+(
+SELECT CompanyGuarantee.fromId               AS fromId,
+       CompanyGuarantee.toId                 AS toId,
+       epoch_ms(CompanyGuarantee.createTime) AS createTime
 FROM CompanyGuarantee
 WHERE CompanyGuarantee.createTime <= :start_date_long
-ORDER BY CompanyGuarantee.createTime
-)
+ORDER BY CompanyGuarantee.createTime )
 TO ':output_dir/snapshot/CompanyGuaranteeCompany.:output_format';
 
 -- PersonOwnAccount. It has deletes.
-COPY (
-SELECT
-    PersonOwnAccount.personId AS personId,
-    PersonOwnAccount.accountId AS accountId,
-    epoch_ms(PersonOwnAccount.createTime) AS createTime
+COPY
+(
+SELECT PersonOwnAccount.personId             AS personId,
+       PersonOwnAccount.accountId            AS accountId,
+       epoch_ms(PersonOwnAccount.createTime) AS createTime
 FROM PersonOwnAccount
-WHERE PersonOwnAccount.createTime <= :start_date_long AND PersonOwnAccount.deleteTime > :start_date_long
-ORDER BY PersonOwnAccount.createTime
-)
+WHERE PersonOwnAccount.createTime <= :start_date_long
+  AND PersonOwnAccount.deleteTime > :start_date_long
+ORDER BY PersonOwnAccount.createTime )
 TO ':output_dir/snapshot/PersonOwnAccount.:output_format';
 
 -- CompanyOwnAccount. It has deletes.
-COPY (
-SELECT
-    CompanyOwnAccount.companyId AS companyId,
-    CompanyOwnAccount.accountId AS accountId,
-    epoch_ms(CompanyOwnAccount.createTime) AS createTime
+COPY
+(
+SELECT CompanyOwnAccount.companyId            AS companyId,
+       CompanyOwnAccount.accountId            AS accountId,
+       epoch_ms(CompanyOwnAccount.createTime) AS createTime
 FROM CompanyOwnAccount
-WHERE CompanyOwnAccount.createTime <= :start_date_long AND CompanyOwnAccount.deleteTime > :start_date_long
-ORDER BY CompanyOwnAccount.createTime
-)
+WHERE CompanyOwnAccount.createTime <= :start_date_long
+  AND CompanyOwnAccount.deleteTime > :start_date_long
+ORDER BY CompanyOwnAccount.createTime )
 TO ':output_dir/snapshot/CompanyOwnAccount.:output_format';
--- tools/convertdata/writes.sql
+++ tools/convertdata/writes.sql
@@ -1,188 +1,204 @@
 -- Inserts
 --- Insert 1: add a person
-COPY (
-SELECT
-    Person.createTime,
-    0 AS dependencyTime,
-    Person.id,
-    Person.name,
-    Person.isBlocked
+COPY
+(
+SELECT Person.createTime,
+       0 AS dependencyTime,
+       Person.id,
+       Person.name,
+       Person.isBlocked
 FROM Person
 WHERE Person.createTime > :start_date_long
-ORDER BY Person.createTime
-)
+ORDER BY Person.createTime )
 TO ':output_dir/inserts/AddPerson.:output_format';
 
 --- Insert 2: add a company
-COPY (
-SELECT
-    Company.createTime,
-    0 AS dependencyTime,
-    Company.id,
-    Company.name,
-    Company.isBlocked
+COPY
+(
+SELECT Company.createTime,
+       0 AS dependencyTime,
+       Company.id,
+       Company.name,
+       Company.isBlocked
 FROM Company
 WHERE Company.createTime > :start_date_long
-ORDER BY Company.createTime
-)
+ORDER BY Company.createTime )
 TO ':output_dir/inserts/AddCompany.:output_format';
 
 
 --- Insert 3: person registers account
-COPY (
-SELECT
-    PersonOwnAccount.createTime,
-    Person.createTime AS dependencyTime,
-    accountId,
-    Account.type AS accountType,
-    Account.isBlocked AS accountBlocked
-FROM Person, Account, PersonOwnAccount
+COPY
+(
+SELECT PersonOwnAccount.createTime,
+       Person.createTime AS dependencyTime,
+       accountId,
+       Account.type      AS accountType,
+       Account.isBlocked AS accountBlocked
+FROM Person,
+     Account,
+     PersonOwnAccount
 WHERE Person.id == PersonOwnAccount.personId
   AND Account.id == PersonOwnAccount.accountId
   AND PersonOwnAccount.createTime > :start_date_long
 ORDER BY PersonOwnAccount.createTime
-)
-TO ':output_dir/inserts/PersonRegister.:output_format';
+    )
+    TO ':output_dir/inserts/PersonRegister.:output_format';
 
 --- Insert 4: company registers account
-COPY (
-SELECT
-    CompanyOwnAccount.createTime,
-    Company.createTime AS dependencyTime,
-    accountId,
-    Account.type AS accountType,
-    Account.isBlocked AS accountBlocked
-FROM Company, Account, CompanyOwnAccount
+COPY
+(
+SELECT CompanyOwnAccount.createTime,
+       Company.createTime AS dependencyTime,
+       accountId,
+       Account.type       AS accountType,
+       Account.isBlocked  AS accountBlocked
+FROM Company,
+     Account,
+     CompanyOwnAccount
 WHERE Company.id == CompanyOwnAccount.companyId
   AND Account.id == CompanyOwnAccount.accountId
   AND CompanyOwnAccount.createTime > :start_date_long
 ORDER BY CompanyOwnAccount.createTime
-)
-TO ':output_dir/inserts/CompanyRegister.:output_format';
+    )
+    TO ':output_dir/inserts/CompanyRegister.:output_format';
 
 --- Insert 5: person invests company
-COPY (
-SELECT
-    PersonInvest.createTime,
-    GREATEST(Person.createTime, Company.createTime) AS dependencyTime,
-    PersonInvest.investorId,
-    PersonInvest.companyId,
-    PersonInvest.ratio AS ratio
-FROM PersonInvest, Person, Company
+COPY
+(
+SELECT PersonInvest.createTime,
+       GREATEST(Person.createTime, Company.createTime) AS dependencyTime,
+       PersonInvest.investorId,
+       PersonInvest.companyId,
+       PersonInvest.ratio                              AS ratio
+FROM PersonInvest,
+     Person,
+     Company
 WHERE PersonInvest.createTime > :start_date_long
-    AND Person.id == PersonInvest.investorId
+  AND Person.id == PersonInvest.investorId
     AND Company.id == PersonInvest.companyId
 ORDER BY PersonInvest.createTime
-)
-TO ':output_dir/inserts/PersonInvest.:output_format';
+    )
+    TO ':output_dir/inserts/PersonInvest.:output_format';
 
 --- Insert 6: company invests company
-COPY (
-SELECT
-    CompanyInvest.createTime,
-    GREATEST(Company1.createTime, Company2.createTime) AS dependencyTime,
-    CompanyInvest.investorId,
-    CompanyInvest.companyId,
-    CompanyInvest.ratio AS ratio
-FROM CompanyInvest, Company AS Company1, Company AS Company2
+COPY
+(
+SELECT CompanyInvest.createTime,
+       GREATEST(Company1.createTime, Company2.createTime) AS dependencyTime,
+       CompanyInvest.investorId,
+       CompanyInvest.companyId,
+       CompanyInvest.ratio                                AS ratio
+FROM CompanyInvest,
+     Company AS Company1,
+     Company AS Company2
 WHERE CompanyInvest.createTime > :start_date_long
-    AND Company1.id == CompanyInvest.investorId
+  AND Company1.id == CompanyInvest.investorId
     AND Company2.id == CompanyInvest.companyId
 ORDER BY CompanyInvest.createTime
-)
-TO ':output_dir/inserts/CompanyInvest.:output_format';
+    )
+    TO ':output_dir/inserts/CompanyInvest.:output_format';
 
 --- Insert 7: person guarantees person
-COPY (
-SELECT
-    PersonGuarantee.createTime,
-    GREATEST(Person1.createTime, Person2.createTime) AS dependencyTime,
-    PersonGuarantee.fromId,
-    PersonGuarantee.toId
-FROM PersonGuarantee, Person AS Person1, Person AS Person2
+COPY
+(
+SELECT PersonGuarantee.createTime,
+       GREATEST(Person1.createTime, Person2.createTime) AS dependencyTime,
+       PersonGuarantee.fromId,
+       PersonGuarantee.toId
+FROM PersonGuarantee,
+     Person AS Person1,
+     Person AS Person2
 WHERE PersonGuarantee.createTime > :start_date_long
-    AND Person1.id == PersonGuarantee.fromId
+  AND Person1.id == PersonGuarantee.fromId
     AND Person2.id == PersonGuarantee.toId
 ORDER BY PersonGuarantee.createTime
-)
-TO ':output_dir/inserts/PersonGuarantee.:output_format';
+    )
+    TO ':output_dir/inserts/PersonGuarantee.:output_format';
 
 --- Insert 8: company guarantees company
-COPY (
-SELECT
-    CompanyGuarantee.createTime,
-    GREATEST(Company1.createTime, Company2.createTime) AS dependencyTime,
-    CompanyGuarantee.fromId,
-    CompanyGuarantee.toId
-FROM CompanyGuarantee, Company AS Company1, Company AS Company2
+COPY
+(
+SELECT CompanyGuarantee.createTime,
+       GREATEST(Company1.createTime, Company2.createTime) AS dependencyTime,
+       CompanyGuarantee.fromId,
+       CompanyGuarantee.toId
+FROM CompanyGuarantee,
+     Company AS Company1,
+     Company AS Company2
 WHERE CompanyGuarantee.createTime > :start_date_long
-    AND Company1.id == CompanyGuarantee.fromId
+  AND Company1.id == CompanyGuarantee.fromId
     AND Company2.id == CompanyGuarantee.toId
 ORDER BY CompanyGuarantee.createTime
-)
-TO ':output_dir/inserts/CompanyGuarantee.:output_format';
+    )
+    TO ':output_dir/inserts/CompanyGuarantee.:output_format';
 
 --- Insert 9: person applies loan
-COPY (
-SELECT
-    PersonApplyLoan.createTime,
-    Person.createTime AS dependencyTime,
-    Loan.loanAmount,
-    Loan.balance,
-FROM PersonApplyLoan, Person, Loan
+COPY
+(
+SELECT PersonApplyLoan.createTime,
+       Person.createTime AS dependencyTime,
+       Loan.loanAmount,
+       Loan.balance,
+FROM PersonApplyLoan,
+     Person,
+     Loan
 WHERE PersonApplyLoan.createTime > :start_date_long
-    AND PersonApplyLoan.personId == Person.id
+  AND PersonApplyLoan.personId == Person.id
     AND PersonApplyLoan.loanId == Loan.id
 ORDER BY PersonApplyLoan.createTime
-)
-TO ':output_dir/inserts/PersonApplyLoan.:output_format';
+    )
+    TO ':output_dir/inserts/PersonApplyLoan.:output_format';
 
 --- Insert 10: company applies loan
-COPY (
-SELECT
-    CompanyApplyLoan.createTime,
-    Company.createTime AS dependencyTime,
-    Loan.loanAmount,
-    Loan.balance,
-FROM CompanyApplyLoan, Company, Loan
+COPY
+(
+SELECT CompanyApplyLoan.createTime,
+       Company.createTime AS dependencyTime,
+       Loan.loanAmount,
+       Loan.balance,
+FROM CompanyApplyLoan,
+     Company,
+     Loan
 WHERE CompanyApplyLoan.createTime > :start_date_long
-    AND CompanyApplyLoan.companyId == Company.id
+  AND CompanyApplyLoan.companyId == Company.id
     AND CompanyApplyLoan.loanId == Loan.id
 ORDER BY CompanyApplyLoan.createTime
-)
-TO ':output_dir/inserts/CompanyApplyLoan.:output_format';
+    )
+    TO ':output_dir/inserts/CompanyApplyLoan.:output_format';
 
 --- Insert 11: Add medium
-COPY (
-SELECT
-    Medium.createTime,
-    0 AS dependencyTime,
-    Medium.id,
-    Medium.type,
-    Medium.isBlocked
+COPY
+(
+SELECT Medium.createTime,
+       0 AS dependencyTime,
+       Medium.id,
+       Medium.type,
+       Medium.isBlocked
 FROM Medium
 WHERE Medium.createTime > :start_date_long
-ORDER BY Medium.createTime
-    )
+ORDER BY Medium.createTime )
 TO ':output_dir/inserts/Medium.:output_format';
 
 --- Insert 12: medium signs in account
-COPY (
-SELECT
-    SignIn.createTime,
-    GREATEST(Account.createTime, Medium.createTime) AS dependencyTime,
-    SignIn.mediumId,
-    SignIn.accountId
-FROM Medium, SignIn, Account
+COPY
+(
+SELECT SignIn.createTime,
+       GREATEST(Account.createTime, Medium.createTime) AS dependencyTime,
+       SignIn.mediumId,
+       SignIn.accountId
+FROM Medium,
+     SignIn,
+     Account
 WHERE SignIn.createTime > :start_date_long
-    AND Medium.id == SignIn.mediumId
+  AND Medium.id == SignIn.mediumId
     AND Account.id == SignIn.accountId
 ORDER BY SignIn.createTime
-)
-TO ':output_dir/inserts/SignIn.:output_format';
+    )
+    TO ':output_dir/inserts/SignIn.:output_format';
 
 --- Insert 13: transfer
-COPY (
+COPY
+(
 (
 SELECT
     Transfer.createTime AS createTime,
@@ -213,58 +229,66 @@ ORDER BY createTime
 TO ':output_dir/inserts/Transfer.:output_format';
 
 --- Insert 14: withdraw
-COPY (
-SELECT
-    Withdraw.createTime,
-    GREATEST(Acc1.createTime, Acc2.createTime) AS dependencyTime,
-    Withdraw.fromId,
-    Withdraw.toId,
-    Withdraw.amount
-FROM Withdraw, Account AS Acc1, Account AS Acc2
+COPY
+(
+SELECT Withdraw.createTime,
+       GREATEST(Acc1.createTime, Acc2.createTime) AS dependencyTime,
+       Withdraw.fromId,
+       Withdraw.toId,
+       Withdraw.amount
+FROM Withdraw,
+     Account AS Acc1,
+     Account AS Acc2
 WHERE Withdraw.createTime > :start_date_long
-    AND Acc1.id == Withdraw.fromId
+  AND Acc1.id == Withdraw.fromId
     AND Acc2.id == Withdraw.toId
 ORDER BY Withdraw.createTime
-)
-TO ':output_dir/inserts/Withdraw.:output_format';
+    )
+    TO ':output_dir/inserts/Withdraw.:output_format';
 
 --- Insert 15: Deposits
-COPY (
-SELECT
-    Deposit.createTime,
-    GREATEST(Account.createTime, Loan.createTime) AS dependencyTime,
-    Deposit.accountId,
-    Deposit.loanId,
-    Deposit.amount
-FROM Deposit, Account, Loan
+COPY
+(
+SELECT Deposit.createTime,
+       GREATEST(Account.createTime, Loan.createTime) AS dependencyTime,
+       Deposit.accountId,
+       Deposit.loanId,
+       Deposit.amount
+FROM Deposit,
+     Account,
+     Loan
 WHERE Deposit.createTime > :start_date_long
-    AND Account.id == Deposit.accountId
+  AND Account.id == Deposit.accountId
     AND Loan.id == Deposit.loanId
 ORDER BY Deposit.createTime
-)
-TO ':output_dir/inserts/Deposit.:output_format';
+    )
+    TO ':output_dir/inserts/Deposit.:output_format';
 
 --- Insert 16: Deposits
-COPY (
-SELECT
-    Repay.createTime,
-    GREATEST(Account.createTime, Loan.createTime) AS dependencyTime,
-    Repay.accountId,
-    Repay.loanId,
-    Repay.amount
-FROM Repay, Account, Loan
+COPY
+(
+SELECT Repay.createTime,
+       GREATEST(Account.createTime, Loan.createTime) AS dependencyTime,
+       Repay.accountId,
+       Repay.loanId,
+       Repay.amount
+FROM Repay,
+     Account,
+     Loan
 WHERE Repay.createTime > :start_date_long
-    AND Account.id == Repay.accountId
+  AND Account.id == Repay.accountId
     AND Loan.id == Repay.loanId
 ORDER BY Repay.createTime
-)
-TO ':output_dir/inserts/Repay.:output_format';
+    )
+    TO ':output_dir/inserts/Repay.:output_format';
 
 
 -- Delete 1: delete an account
-COPY (SELECT deleteTime, createTime as dependentDate, id
-      FROM Account
-      WHERE deleteTime > :start_date_long
-        AND isExplicitDeleted = true
-      ORDER BY deleteTime ASC)
+COPY
+(
+SELECT deleteTime, createTime as dependentDate, id
+FROM Account
+WHERE deleteTime > :start_date_long
+  AND isExplicitDeleted = true
+ORDER BY deleteTime ASC)
 TO ':output_dir/deletes/Account.:output_format';
