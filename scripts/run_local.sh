c57a5a820c19f49dc2a925f7c34cc7c8428bebf5
hujiatao <hujiatao@stargraph.cn>
2023-12-04 22:43:55 +0800

    tcr8 parameter curation

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -5,5 +5,4 @@ OUTPUT_DIR=out
 
 # For more command line arguments, see the main entry for more information at
 # src/main/scala/ldbc/finbench/datagen/LdbcDatagen.scala
-python3 scripts/run.py --jar $LDBC_SNB_DATAGEN_JAR --main-class ldbc.finbench.datagen.LdbcDatagen --memory 100g
-  -- --scale-factor 0.1 --output-dir ${OUTPUT_DIR}
+python3 scripts/run.py --jar $LDBC_SNB_DATAGEN_JAR --main-class ldbc.finbench.datagen.LdbcDatagen --memory 10g -- --scale-factor 0.1 --output-dir ${OUTPUT_DIR}
