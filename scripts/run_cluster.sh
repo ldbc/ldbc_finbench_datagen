12b50446024a32eabc27137d742605a893f2ca3a
qishipengqsp <shepherdkingqsp@gmail.com>
2024-07-23 20:42:16 +0800

    format generators

--- scripts/run_cluster.sh
+++ scripts/run_cluster.sh
@@ -6,12 +6,7 @@ OUTPUT_DIR=/tmp/finbench-out/
 echo "start: " `date`
 
 # Run Spark Application
-#spark-submit --class org.apache.spark.examples.SparkPi  --master spark://finbench-large-00:7077 --num-executors 2
-#--conf "spark.default.parallelism=2" ~/software/spark-3.2.2-bin-hadoop3.2/examples/jars/spark-examples_2.12-3.2.2.jar
-#10000
-
-time spark-submit --master spark://finbench-large-00:7077 --class ldbc.finbench.datagen.LdbcDatagen ${LDBC_FINBENCH_DATAGEN_JAR} --scale-factor 1 --output-dir ${OUTPUT_DIR}
-
+time spark-submit --master spark://finbench-large-00:7077 --class ldbc.finbench.datagen.LdbcDatagen --driver-memory 100g ${LDBC_FINBENCH_DATAGEN_JAR} --scale-factor 1 --output-dir ${OUTPUT_DIR}
 #spark-submit --master spark://finbench-large-00:7077 --class ldbc.finbench.datagen.LdbcDatagen --num-executors 2 --conf "spark.default.parallelism=2"   /home/qsp/github-projects/ldbc_finbench_datagen/target/ldbc_finbench_datagen-0.2.0-SNAPSHOT-jar-with-dependencies.jar --scale-factor 1 --output-dir /tmp/finbench-out/
 
 echo "End: " `date`
--- src/main/scala/ldbc/finbench/datagen/generation/generators/SparkCompanyGenerator.scala
+++ src/main/scala/ldbc/finbench/datagen/generation/generators/SparkCompanyGenerator.scala
@@ -9,26 +9,35 @@ import org.apache.spark.sql.SparkSession
 import scala.collection.JavaConverters.asScalaIteratorConverter
 
 object SparkCompanyGenerator {
-  def apply(companyNums: Long, config: DatagenConfiguration, blockSize: Int, numPartitions: Option[Int] = None)(
-    implicit spark: SparkSession): RDD[Company] = {
-    // OPT: It is called in each SparkGenerator in Spark to initialize the context on the executors.
-    // 1. Make the context as an object instead of a static class
-    // 2. Pass the context to SparkContext instead of 
-    DatagenContext.initialize(config)
+  def apply(
+      companyNums: Long,
+      config: DatagenConfiguration,
+      blockSize: Int,
+      numPartitions: Option[Int] = None
+  )(implicit spark: SparkSession): RDD[Company] = {
 
     val numBlocks = Math.ceil(companyNums / blockSize.toDouble).toInt
 
     val companyPartitionGenerator = (blocks: Iterator[Long]) => {
+      // OPT: It is called in each SparkGenerator in Spark to initialize the context on the executors.
+      // 1. Make the context as an object instead of a static class
+      // 2. Pass the context to SparkContext instead of
+      DatagenContext.initialize(config)
+
       val companyGenerator = new CompanyGenerator()
 
       for {
         i <- blocks
         size = Math.min(companyNums - blockSize * i, blockSize)
-        company <- companyGenerator.generateCompanyBlock(i.toInt, blockSize).asScala.take(size.toInt)
+        company <- companyGenerator
+          .generateCompanyBlock(i.toInt, blockSize)
+          .asScala
+          .take(size.toInt)
       } yield company
     }
 
-    val partitions = numPartitions.getOrElse(spark.sparkContext.defaultParallelism)
+    val partitions =
+      numPartitions.getOrElse(spark.sparkContext.defaultParallelism)
 
     spark.sparkContext
       .range(0, numBlocks, step = 1, numSlices = partitions)
