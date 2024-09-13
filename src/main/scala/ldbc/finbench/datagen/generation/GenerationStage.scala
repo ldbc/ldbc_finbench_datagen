1da63f3ded1ae6ff1c288848a2b048b904725493
qishipengqsp <shepherdkingqsp@gmail.com>
2024-09-13 17:11:42 +0800

    wip

--- src/main/scala/ldbc/finbench/datagen/generation/GenerationStage.scala
+++ src/main/scala/ldbc/finbench/datagen/generation/GenerationStage.scala
@@ -48,16 +48,10 @@ object GenerationStage extends DatagenStage with Logging {
         )
     }
 
-    // TODO: It's better to define multiple job groups.
-    SparkUI.job(
-      implicitly[ClassTag[ActivitySimulator]].runtimeClass.getSimpleName,
-      "serialize Finbench data"
-    ) {
-      val simulator = new ActivitySimulator(
-        RawSink(args.outputDir, format, args.partitionsOpt)
-      )
-      simulator.simulate(config)
-    }
+    val simulator = new ActivitySimulator(
+      RawSink(args.outputDir, format, args.partitionsOpt)
+    )
+    simulator.simulate(config)
   }
 
   private def buildConfig(args: Args): DatagenConfiguration = {
