fd05df279383b5be9ff0a9281fb479f956731705
qishipengqsp <shepherdkingqsp@gmail.com>
2024-10-16 18:33:29 +0800

    update scale factor params

---
+++ scripts/run_paramgen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LDBC_FINBENCH_DATAGEN_JAR=target/ldbc_finbench_datagen-0.2.0-SNAPSHOT-jar-with-dependencies.jar
+OUTPUT_DIR=out/sf10/
+
+# Note: generate factor tables with --generate-factors
+
+time spark-submit --master local[*] \
+    --class ldbc.finbench.datagen.LdbcDatagen \
+    --driver-memory 480g \
+    ${LDBC_FINBENCH_DATAGEN_JAR} \
+    --output-dir ${OUTPUT_DIR} \
+    --factor-format csv \
+    --generate-factors
+
+echo "start parameter curation"
+
+python3
